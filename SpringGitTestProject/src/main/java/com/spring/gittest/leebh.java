059f17fb1abadbab8ca57ab3022d08d58c2f4021
user1 <user1@DESKTOP-8UC660Q>
2020-01-08 12:39:34 +0900

    2020.01.08.수 - 12:39 pm 이병희 System.out.println("이병희 - 실험중입니다.");

---
+++ SpringGitTestProject/src/main/java/com/spring/gittest/leebh.java
@@ -0,0 +1,14 @@
+package com.spring.gittest;
+
+public class leebh {
+
+	public static void main(String[] args) {
+		
+		// 2020.01.08.수 - 12:39 pm
+		System.out.println("이병희 - 실험중입니다.");
+		
+		
+
+	}
+
+}
